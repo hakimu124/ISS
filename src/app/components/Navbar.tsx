fa7f934851bb0b02e61aa09f9581c04360591f0e
Abdihakma <abdihakma0@gmail.com>
2026-05-22 18:29:50 +0300

    Remove unused Image imports to fix Netlify build error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 // Using text-based logo instead of image
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
